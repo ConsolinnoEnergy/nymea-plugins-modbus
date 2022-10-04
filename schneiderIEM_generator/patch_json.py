adc6142301217152ac8948405710dcdf50c4849c
m.bettermann <m.bettermann@consolinno.de>
2022-10-04 10:39:41 +0200

    add schneiderIEM_generator

---
+++ schneiderIEM_generator/patch_json.py
@@ -0,0 +1,60 @@
+import json
+import argparse
+import uuid
+
+
+def traverse_json(data, cb, uuid_offset):
+    for key, value in data.items():
+        cb(key, value, data, uuid_offset)
+        if isinstance(value, dict):
+            traverse_json(value, cb, uuid_offset)
+        elif isinstance(value, list):
+            for val in value:
+                if isinstance(val, str):
+                    pass
+                elif isinstance(val, list):
+                    pass
+                else:
+                    traverse_json(val, cb, uuid_offset)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generates a patched SchneiderIEM JSON plugin info.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("JSON", help="Original JSON file")
+    parser.add_argument("--output", help="Outputfile")
+    parser.add_argument(
+        "--interfaces", help="Interfaces of new SchneiderIEM plugin", required=True, nargs="+"
+    )
+    #    parser.add_argument(
+    #        "--name", help="Name of new SchneiderIEM thing class", required=True
+    #    )
+    parser.add_argument(
+        "--display_name", help="Display name of new SchneiderIEM thing", required=True
+    )
+    parser.add_argument(
+        "--uuid_offset", type=int, help="Offset of uuids of new plugin ", default=1
+    )
+    args = parser.parse_args()
+    with open(args.JSON, "r") as f:
+        data = json.loads(f.read())
+
+    def callback(key, value, data, uuid_offset):
+        if key == "id":
+            tmp = list(data[key])
+            # Adding offset to ascii int of last character of uuid
+            new = f"%03d" % (ord(tmp[-1]) + 1)
+            tmp[-3] = new[0]
+            tmp[-2] = new[1]
+            tmp[-1] = new[2]
+            data[key] = "".join(tmp)
+
+    traverse_json(data, callback, args.uuid_offset)
+    # data['vendors'][0]['thingClasses'][0]['name'] = args.name
+    data["vendors"][0]["thingClasses"][0]["displayName"] = args.display_name
+    data["vendors"][0]["thingClasses"][0]["interfaces"] = args.interfaces
+
+    with open(args.output, "w") as f:
+        json.dump(data, f, indent=4)
---
+++ schneiderIEM_generator/schneiderIEM_generator.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#Name of the generator
+GENERATORNAME=schneiderIEM_generator
+
+#SOURCE Integration you would like to copy
+SOURCE_INTEGRATION=schneiderIEM
+
+# Target files, meaning the needed integration files.
+TARGET=schneiderIEM_inverter
+TARGETJSON=integrationpluginschneiderIEM_inverter.json
+TARGETH=integrationpluginschneiderIEM_inverter.h
+TARGETCPP=integrationpluginschneiderIEM_inverter.cpp
+TARGETPRO=schneiderIEM_inverter.pro
+
+# JSON file variables to replace
+DISPLAYNAME="SchneiderIEM Inverter"
+INTERFACE1=solarinverter
+INTERFACE2=connectable
+
+# Name of the package (debian package variables)
+SOURCEPACKAGENAME=nymea-plugin-bgetech
+PACKAGENAME=nymea-plugin-schneider-iem-inverter
+DEVICENAME=schneiderIEM
+
+
+cp -r ../$SOURCE_INTEGRATION ../$TARGET
+cd ../$TARGET
+rename "s/${SOURCE_INTEGRATION}/${TARGET}/g" ./*
+python3 ../$GENERATORNAME/patch_json.py $TARGETJSON --interfaces $INTERFACE1 $INTERFACE2 --output $TARGETJSON  --display_name "${DISPLAYNAME}"
+uuid=$(cat $TARGETJSON | grep "id" | head -n 1 | sed 's/"id": "//' | sed 's/",//g' | xargs)
+mv translations/*-de.ts translations/$uuid-de.ts 
+mv translations/*-de_DE.ts translations/$uuid-de_DE.ts 
+mv translations/*-en_US.ts translations/$uuid-en_US.ts 
+
+sed "s/${SOURCE_INTEGRATION}\.json/${TARGET}\.json/g" $TARGETH -i
+sed "s/${SOURCE_INTEGRATION}\.h/${TARGET}\.h/g" $TARGETCPP -i
+sed "s/${SOURCE_INTEGRATION}\./${TARGET}\./g" $TARGETPRO -i
+cd ..
+sed "s/${SOURCE_INTEGRATION}/${SOURCE_INTEGRATION} \\\ \n    ${TARGET}/g" nymea-plugins-modbus.pro -i
+
+tee -a debian/control <<EOF
+
+Package: $PACKAGENAME
+Architecture: any
+Section: libs
+Depends: \${shlibs:Depends},
+         \${misc:Depends},
+Description: nymea integration to use $DEVICENAME as solar inverter metering device
+ This package contains the nymea integration plugin to use a  $DEVICENAME as a solar inverter metering device.
+EOF
+
+cp debian/$SOURCEPACKAGENAME.install.in debian/$PACKAGENAME.install.in
+sed 's/${SOURCE}/${TARGET}/g' debian/$PACKAGENAME.install.in -i
