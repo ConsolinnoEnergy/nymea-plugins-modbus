8daed251d3c76872ebb1d26d35345e7dc707681b
l.heizinger <l.heizinger@consolinno.de>
2023-03-15 12:38:31 +0000

    Merge branch 'add-kaco-modbus' into 'main'
    Kaco SunSpec Plugin
    
    See merge request leafletfirmware/nymea/nymea-plugins-modbus!58
    
    (cherry picked from commit 8f039abf1f8bbec8738e68d7005105adf04bacda)
    
    f47b11ff Plugin for Kaco Sunspec inverters, Modbus TCP and RTU. WIP
    119d0068 Should be done, but somehow does not work. No idea why. No debian packages...
    bba222a3 Problems fixed, should work now.
    cf5f0b13 Update timer changed from 5s -> 2s, tested and works fine.
    0c9e825b Updated copyright notice.
    51b09805 Updated readme.
    76808e69 Resolved naming conflict with other Kaco plugin. Debian package now available...

---
+++ kacosunspec/translations/5168c92c-fe0c-4716-aba2-24ec886af885-en_US.ts
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>IntegrationPluginKacoSunSpec</name>
+    <message>
+        <location filename="../integrationpluginkacosunspec.cpp" line="59"/>
+        <source>The network device discovery is not available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginkacosunspec.cpp" line="107"/>
+        <source>No Modbus RTU interface available. Please set up a Modbus RTU interface first.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginkacosunspec.cpp" line="113"/>
+        <source>The Modbus slave address must be a value between 3 and 247.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginkacosunspec.cpp" line="160"/>
+        <source>The MAC address is not vaild. Please reconfigure the device to fix this.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginkacosunspec.cpp" line="197"/>
+        <source>Could not initialize the communication with the inverter.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginkacosunspec.cpp" line="246"/>
+        <source>The Modbus address not valid. It must be a value between 3 and 247.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginkacosunspec.cpp" line="253"/>
+        <source>The Modbus RTU resource is not available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KacoSunSpec</name>
+    <message>
+        <location filename="../plugininfo.h" line="43"/>
+        <location filename="../plugininfo.h" line="46"/>
+        <source>Active power</source>
+        <extracomment>The name of the StateType ({1d735719-f10c-4230-9bc0-3996ee68bee4}) of ThingClass kacosunspecInverterRTU
+----------
+The name of the StateType ({bb8cdb1e-3612-42b4-a995-2b3179cbb741}) of ThingClass kacosunspecInverterTCP</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="49"/>
+        <location filename="../plugininfo.h" line="52"/>
+        <source>Connected</source>
+        <extracomment>The name of the StateType ({e5a8e0e1-cd98-4c5e-9271-d4c33e1c028c}) of ThingClass kacosunspecInverterRTU
+----------
+The name of the StateType ({dd6739c5-c7c9-4ebf-902b-0f3b2cbaa0f6}) of ThingClass kacosunspecInverterTCP</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="55"/>
+        <source>Kaco Inverter (Modbus RTU)</source>
+        <extracomment>The name of the ThingClass ({4fa73aab-377f-41ce-b167-14654f157b88})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="58"/>
+        <source>Kaco Inverter (Modbus TCP)</source>
+        <extracomment>The name of the ThingClass ({2c6f9d45-44a2-4a16-96d8-76be4162ffcf})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="61"/>
+        <location filename="../plugininfo.h" line="64"/>
+        <source>Kaco SunSpec</source>
+        <extracomment>The name of the vendor ({41bd21a1-6c34-4a2a-907c-859ce0efc012})
+----------
+The name of the plugin KacoSunSpec ({5168c92c-fe0c-4716-aba2-24ec886af885})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="67"/>
+        <source>MAC address</source>
+        <extracomment>The name of the ParamType (ThingClass: kacosunspecInverterTCP, Type: thing, ID: {9f96f308-dca3-438f-ac4a-7dde0c5f49f1})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="70"/>
+        <source>Modbus RTU master</source>
+        <extracomment>The name of the ParamType (ThingClass: kacosunspecInverterRTU, Type: thing, ID: {8ccaf129-c303-49a3-be4f-d891f0bff988})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="73"/>
+        <source>Modbus slave address</source>
+        <extracomment>The name of the ParamType (ThingClass: kacosunspecInverterRTU, Type: thing, ID: {c5cb32d8-2811-4610-8f7e-1edb55f58729})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="76"/>
+        <location filename="../plugininfo.h" line="79"/>
+        <source>Operating State</source>
+        <extracomment>The name of the StateType ({f45d0dbf-6947-45e4-a67e-89e646a2d3b0}) of ThingClass kacosunspecInverterRTU
+----------
+The name of the StateType ({a7bb1db5-1100-4ddc-acd6-3b9e325d1977}) of ThingClass kacosunspecInverterTCP</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="82"/>
+        <source>Port</source>
+        <extracomment>The name of the ParamType (ThingClass: kacosunspecInverterTCP, Type: thing, ID: {a2894fe9-8e2a-4d47-9443-90cb71b6d527})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="85"/>
+        <source>Slave ID</source>
+        <extracomment>The name of the ParamType (ThingClass: kacosunspecInverterTCP, Type: thing, ID: {c34a8776-3e90-41c4-ae32-fb461e5cb998})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="88"/>
+        <source>Slave address</source>
+        <extracomment>The name of the ParamType (ThingClass: kacosunspecInverterRTU, Type: discovery, ID: {95f393de-13bc-4e32-b538-0c9f097e4c75})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="91"/>
+        <location filename="../plugininfo.h" line="94"/>
+        <source>Total energy produced</source>
+        <extracomment>The name of the StateType ({2c7c7a2c-7af6-4038-a6df-47e2e0026f97}) of ThingClass kacosunspecInverterRTU
+----------
+The name of the StateType ({057c00ca-ee8e-4914-b618-e686e2a48906}) of ThingClass kacosunspecInverterTCP</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
