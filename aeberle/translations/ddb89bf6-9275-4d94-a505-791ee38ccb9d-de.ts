017a69ad8298fdeefb1a88c868bb89849d627aa6
Cornelius Kast <cornelius.kast@enqs.de>
2023-06-12 17:11:30 +0200

    #26: add translation files

---
+++ aeberle/translations/ddb89bf6-9275-4d94-a505-791ee38ccb9d-de.ts
@@ -0,0 +1,339 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>IntegrationPluginAEberle</name>
+    <message>
+        <location filename="../integrationpluginaeberle.cpp" line="75"/>
+        <source>No Modbus RTU interface available. Please set up the Modbus RTU interface first.</source>
+        <translation>Keine Modbus RTU Schnittstelle verfügbar. Bitte richte zuerst die Modbus RTU Schnittstelle ein.</translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginaeberle.cpp" line="82"/>
+        <location filename="../integrationpluginaeberle.cpp" line="104"/>
+        <location filename="../integrationpluginaeberle.cpp" line="126"/>
+        <source>The Modbus slave address must be a value between 1 and 254.</source>
+        <translation>Die Modbus Adresse muss ein Wert zwischen 1 und 254 sein.</translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginaeberle.cpp" line="159"/>
+        <location filename="../integrationpluginaeberle.cpp" line="269"/>
+        <location filename="../integrationpluginaeberle.cpp" line="355"/>
+        <source>The Modbus address not valid. It must be a value between 1 and 254.</source>
+        <translation>Die Modbus Adresse is ungültig. Sie muss ein Wert zwischen 1 und 254 sein.</translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginaeberle.cpp" line="166"/>
+        <location filename="../integrationpluginaeberle.cpp" line="276"/>
+        <location filename="../integrationpluginaeberle.cpp" line="362"/>
+        <source>The Modbus RTU interface not available.</source>
+        <translation>Die Modbus RTU Schnittstelle ist nicht verfügbar.</translation>
+    </message>
+</context>
+<context>
+    <name>aEberle</name>
+    <message>
+        <location filename="../plugininfo.h" line="99"/>
+        <location filename="../plugininfo.h" line="102"/>
+        <source>A-Eberle</source>
+        <extracomment>The name of the vendor ({49bd90dd-e1ac-4e30-bc75-648d8bfb8d26})
+----------
+The name of the plugin aEberle ({ddb89bf6-9275-4d94-a505-791ee38ccb9d})</extracomment>
+        <translation>A-Eberle</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="105"/>
+        <location filename="../plugininfo.h" line="108"/>
+        <location filename="../plugininfo.h" line="111"/>
+        <source>Connected</source>
+        <extracomment>The name of the StateType ({c1112af4-c3e1-4bf7-9e46-d272ee5ae48e}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({67bf1d42-7d6c-4f21-9c81-5718dd702e82}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({e3b30fa1-b074-4d83-a30d-2edc7fd04185}) of ThingClass pqida</extracomment>
+        <translation>Verbunden</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="114"/>
+        <location filename="../plugininfo.h" line="117"/>
+        <location filename="../plugininfo.h" line="120"/>
+        <source>Current phase A</source>
+        <extracomment>The name of the StateType ({f710212c-6699-4c95-9dfc-af09ed7f1b6f}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({f394ce30-1f65-4321-8197-bae9ef8578ea}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({ab9271ac-0a50-48ba-8097-53890c3a3172}) of ThingClass pqida</extracomment>
+        <translation>Strom Phase A</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="123"/>
+        <location filename="../plugininfo.h" line="126"/>
+        <location filename="../plugininfo.h" line="129"/>
+        <source>Current phase B</source>
+        <extracomment>The name of the StateType ({4b20aa18-67ff-4359-a33a-24403f961a89}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({89027dc7-7230-43ca-a101-9c0388ce970d}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({309bcde4-4bff-415c-84fe-e41724030549}) of ThingClass pqida</extracomment>
+        <translation>Strom Phase B</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="132"/>
+        <location filename="../plugininfo.h" line="135"/>
+        <location filename="../plugininfo.h" line="138"/>
+        <source>Current phase C</source>
+        <extracomment>The name of the StateType ({765f8f65-0332-44cc-997a-fb8f65678f59}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({13650538-480e-4ed0-90ed-1b5794dfd2b6}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({dfa1f223-469f-43a6-807b-12914c83f489}) of ThingClass pqida</extracomment>
+        <translation>Strom Phase C</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="141"/>
+        <location filename="../plugininfo.h" line="144"/>
+        <location filename="../plugininfo.h" line="147"/>
+        <source>Current power</source>
+        <extracomment>The name of the StateType ({d8cf1a3a-b21f-4c36-ad74-c5418f42966c}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({ccc7b6db-e56a-450b-8516-2b2bf989d44a}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({b337d845-d8f9-4207-a7c9-287e5d8b18af}) of ThingClass pqida</extracomment>
+        <translation>Aktuelle Leistung</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="150"/>
+        <location filename="../plugininfo.h" line="153"/>
+        <location filename="../plugininfo.h" line="156"/>
+        <source>Current power phase A</source>
+        <extracomment>The name of the StateType ({2d78aead-ffcb-4e77-a197-aa963be9743a}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({2ed46061-ec05-4a56-9185-1a226bc3330e}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({42ccd07a-befe-4f39-af0b-0b7e18db0ae3}) of ThingClass pqida</extracomment>
+        <translation>Aktuelle Leistung Phase A</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="159"/>
+        <location filename="../plugininfo.h" line="162"/>
+        <location filename="../plugininfo.h" line="165"/>
+        <source>Current power phase B</source>
+        <extracomment>The name of the StateType ({31ac779a-87c9-4a6c-b3ad-ec79d9402276}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({2fb6ca94-8b67-471b-bcd0-6a0380d5bb1f}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({91af6dad-de95-4192-b2c5-873e9d85220c}) of ThingClass pqida</extracomment>
+        <translation>Aktuelle Leistung Phase B</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="168"/>
+        <location filename="../plugininfo.h" line="171"/>
+        <location filename="../plugininfo.h" line="174"/>
+        <source>Current power phase C</source>
+        <extracomment>The name of the StateType ({0eeb6f9d-f76d-4369-9f10-0c18eae38e39}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({e59f1eb5-1b2d-4862-890d-3f760efcec18}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({204913dc-4fe9-44ca-a4e0-4d0884fe5942}) of ThingClass pqida</extracomment>
+        <translation>Aktuelle Leistung Phase C</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="177"/>
+        <location filename="../plugininfo.h" line="180"/>
+        <location filename="../plugininfo.h" line="183"/>
+        <source>Energy consumed phase A</source>
+        <extracomment>The name of the StateType ({2dbd0d30-bd2d-49e1-9c32-cc84197e630f}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({2f51c363-37b5-43cb-8f2a-ef5d80aa84fe}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({0dab8b3c-0cee-4838-8c25-70eee8b5254e}) of ThingClass pqida</extracomment>
+        <translation>Gesamtenergieverbrauch Phase A</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="186"/>
+        <location filename="../plugininfo.h" line="189"/>
+        <location filename="../plugininfo.h" line="192"/>
+        <source>Energy consumed phase B</source>
+        <extracomment>The name of the StateType ({7ff009f0-8270-43dc-af32-31d8cf054115}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({3476ff30-6e66-43fa-896a-60567cf30051}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({4395c1b7-4c85-4b40-b949-218c437bb578}) of ThingClass pqida</extracomment>
+        <translation>Gesamtenergieverbrauch Phase B</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="195"/>
+        <location filename="../plugininfo.h" line="198"/>
+        <location filename="../plugininfo.h" line="201"/>
+        <source>Energy consumed phase C</source>
+        <extracomment>The name of the StateType ({814a9147-c2e6-4f4b-a9eb-933719cf2dd1}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({d0a356e9-6927-437c-87d2-9f610fd23f33}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({869cd304-c9b3-4de5-a10c-d12eccab34db}) of ThingClass pqida</extracomment>
+        <translation>Gesamtenergieverbrauch Phase C</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="204"/>
+        <location filename="../plugininfo.h" line="207"/>
+        <location filename="../plugininfo.h" line="210"/>
+        <source>Energy produced phase A</source>
+        <extracomment>The name of the StateType ({770e3cd1-c29c-43c4-8129-ed0f6d964725}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({0c158215-f5b8-4824-b20e-1fc6dc55e74f}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({6a78e62c-acd6-4497-8aed-2804b11fccce}) of ThingClass pqida</extracomment>
+        <translation>Erzeugte Energie Phase A</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="213"/>
+        <location filename="../plugininfo.h" line="216"/>
+        <location filename="../plugininfo.h" line="219"/>
+        <source>Energy produced phase B</source>
+        <extracomment>The name of the StateType ({256c5af7-d606-4ac1-9173-b4f3242c0b3b}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({940a968e-bcb8-477c-a7ee-3ce8599ddd0e}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({22c73a45-b943-4746-b547-f0ec9aba21d4}) of ThingClass pqida</extracomment>
+        <translation>Erzeugte Energie Phase B</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="222"/>
+        <location filename="../plugininfo.h" line="225"/>
+        <location filename="../plugininfo.h" line="228"/>
+        <source>Energy produced phase C</source>
+        <extracomment>The name of the StateType ({010690f8-4086-4608-8f53-f2818e602c5b}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({2a7a3727-599c-42a8-b7c0-0f5d52a552ef}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({7a2745a7-4d8d-4306-989b-4efb2bd2a22d}) of ThingClass pqida</extracomment>
+        <translation>Erzeugte Energie Phase C</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="231"/>
+        <location filename="../plugininfo.h" line="234"/>
+        <location filename="../plugininfo.h" line="237"/>
+        <source>Frequency</source>
+        <extracomment>The name of the StateType ({64b59136-4710-43f7-ac55-7c5d94e973f8}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({bb24327f-1231-46d2-8389-2f4d80a759f8}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({dd8d3d90-01ec-4a49-8660-9b379adaa9fb}) of ThingClass pqida</extracomment>
+        <translation>Frequenz</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="240"/>
+        <location filename="../plugininfo.h" line="243"/>
+        <location filename="../plugininfo.h" line="246"/>
+        <source>Modbus RTU master</source>
+        <extracomment>The name of the ParamType (ThingClass: pqida_consumer, Type: thing, ID: {f43bf0c5-e912-46fb-a146-aa22b2ab485c})
+----------
+The name of the ParamType (ThingClass: pqida_inverter, Type: thing, ID: {a5d55469-87dc-4f03-b997-07d649ae19d8})
+----------
+The name of the ParamType (ThingClass: pqida, Type: thing, ID: {9d343bf2-1371-44d1-9fe0-6a897c0beb60})</extracomment>
+        <translation>Modbus RTU Master</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="249"/>
+        <location filename="../plugininfo.h" line="252"/>
+        <location filename="../plugininfo.h" line="255"/>
+        <source>Modbus slave address</source>
+        <extracomment>The name of the ParamType (ThingClass: pqida_consumer, Type: thing, ID: {66679174-4a6d-4eba-b48d-ed61044eb7f5})
+----------
+The name of the ParamType (ThingClass: pqida_inverter, Type: thing, ID: {45077f42-dfdf-4e41-a922-2f4bae389411})
+----------
+The name of the ParamType (ThingClass: pqida, Type: thing, ID: {7ca13c72-6d9c-49d7-8f0d-a1a1b07fb855})</extracomment>
+        <translation>Modbus Adresse</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="258"/>
+        <source>PQI-DA smart</source>
+        <extracomment>The name of the ThingClass ({c602b701-beea-4237-bc0e-6a8a09c876e9})</extracomment>
+        <translation>PQI-DA smart</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="261"/>
+        <source>PQI-DA smart (Consumer)</source>
+        <extracomment>The name of the ThingClass ({c3fcc60a-e673-45dd-a614-c26e68e7d012})</extracomment>
+        <translation>PQI-DA smart (Verbraucher)</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="264"/>
+        <source>PQI-DA smart (Inverter)</source>
+        <extracomment>The name of the ThingClass ({5c89a59b-ce47-4e4f-9ed7-f9ea3dfba6d6})</extracomment>
+        <translation>PQI-DA smart (Wechselrichter)</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="267"/>
+        <location filename="../plugininfo.h" line="270"/>
+        <location filename="../plugininfo.h" line="273"/>
+        <source>Slave address</source>
+        <extracomment>The name of the ParamType (ThingClass: pqida_consumer, Type: discovery, ID: {b47e2cc8-cdd2-4506-ac64-d67a3fd0a851})
+----------
+The name of the ParamType (ThingClass: pqida_inverter, Type: discovery, ID: {9d841b4b-8cae-411c-92f6-8a3b8050b79e})
+----------
+The name of the ParamType (ThingClass: pqida, Type: discovery, ID: {7fe9236e-9985-46b4-9ec2-a89fa6533467})</extracomment>
+        <translation>Modbus Adresse</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="276"/>
+        <location filename="../plugininfo.h" line="279"/>
+        <location filename="../plugininfo.h" line="282"/>
+        <source>Total energy consumed</source>
+        <extracomment>The name of the StateType ({77f8e618-1846-4300-8b2f-38b8a89cdf91}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({51100815-6510-4c7c-917c-b072962c0dc3}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({3da03f2c-0a47-463b-9cb3-7b1a70a7dd5d}) of ThingClass pqida</extracomment>
+        <translation>Gesamtenergieverbrauch</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="285"/>
+        <location filename="../plugininfo.h" line="288"/>
+        <location filename="../plugininfo.h" line="291"/>
+        <source>Total energy produced</source>
+        <extracomment>The name of the StateType ({53f87af4-bf35-4413-9151-65d9e3dedd98}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({bf05c433-60f9-4818-98e1-3d1abfa5a4a2}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({34931f6e-33d7-46d6-bf03-acc3912e9792}) of ThingClass pqida</extracomment>
+        <translation>Gesamte erzeugte Energie</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="294"/>
+        <location filename="../plugininfo.h" line="297"/>
+        <location filename="../plugininfo.h" line="300"/>
+        <source>Voltage phase A</source>
+        <extracomment>The name of the StateType ({c55683ba-80ff-4494-8d1b-92820e999f5f}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({114f2cfe-8c64-4ca3-9890-cddd0e8389ff}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({44015d59-aa17-4116-8027-bdb984431aeb}) of ThingClass pqida</extracomment>
+        <translation>Spannung Phase A</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="303"/>
+        <location filename="../plugininfo.h" line="306"/>
+        <location filename="../plugininfo.h" line="309"/>
+        <source>Voltage phase B</source>
+        <extracomment>The name of the StateType ({cd27d7aa-b3ee-4d7d-9c83-66566459d6ea}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({f9930f35-0767-4271-b57a-6a57c2b570fc}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({d7e32a87-4c05-48ab-8769-ee1989281758}) of ThingClass pqida</extracomment>
+        <translation>Spannung Phase B</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="312"/>
+        <location filename="../plugininfo.h" line="315"/>
+        <location filename="../plugininfo.h" line="318"/>
+        <source>Voltage phase C</source>
+        <extracomment>The name of the StateType ({9a8aad96-8396-4096-95ab-d4aaf5938b32}) of ThingClass pqida_consumer
+----------
+The name of the StateType ({6cd62a7d-54a1-4619-ba0d-af1248194de1}) of ThingClass pqida_inverter
+----------
+The name of the StateType ({0a73aaf3-0fb3-4805-b99b-f7329742ff71}) of ThingClass pqida</extracomment>
+        <translation>Spannung Phase C</translation>
+    </message>
+</context>
+</TS>
