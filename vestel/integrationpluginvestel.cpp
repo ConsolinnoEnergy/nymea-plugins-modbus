d8f5ba724eb2015a563783a4c68210a69f59b214
s.ringer <s.ringer@consolinno.de>
2023-10-24 18:15:40 +0200

    Another small fix.

--- vestel/integrationpluginvestel.cpp
+++ vestel/integrationpluginvestel.cpp
@@ -296,9 +296,9 @@ void IntegrationPluginVestel::setupEVC04Connection(ThingSetupInfo *info)
             quint16 count = m_timeoutCount[thing];
             count++;
             m_timeoutCount[thing] = count;
-            qCWarning(dcWebasto()) << "Time value did not update, count" << count;
+            qCWarning(dcVestel()) << "Time value did not update, count" << count;
             if (count >= 3) {
-                qCWarning(dcWebasto()) << "Time value did not update three times. Wallbox seems stuck and returning outdated values. Reconnecting...";
+                qCWarning(dcVestel()) << "Time value did not update three times. Wallbox seems stuck and returning outdated values. Reconnecting...";
                 evc04Connection->reconnectDevice();
             }
         } else {
