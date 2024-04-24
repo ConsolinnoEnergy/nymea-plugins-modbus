08f53c38ef800c73c93fb1f8f165f9c49accea2c
Luis Reber <l.reber@consolinno.de>
2024-04-24 15:25:18 +0200

    Added translations

---
+++ solaxevc/translations/solaxevc-en_US.ts
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>IntegrationPluginSolaxEvc</name>
+    <message>
+        <location filename="../integrationpluginsolaxevc.cpp" line="50"/>
+        <source>The network device discovery is not available.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginsolaxevc.cpp" line="113"/>
+        <source>The MAC address is not vaild. Please reconfigure the device to fix this.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>solaxEvc</name>
+    <message>
+        <location filename="../plugininfo.h" line="53"/>
+        <source>Active power</source>
+        <extracomment>The name of the StateType ({b060adab-02b8-4892-aa51-18e5eaab852f}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="56"/>
+        <source>Charging</source>
+        <extracomment>The name of the StateType ({d4c499dc-f0fd-463d-acdd-b7151f3baf91}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="59"/>
+        <location filename="../plugininfo.h" line="62"/>
+        <source>Charging enabled</source>
+        <extracomment>The name of the ParamType (ThingClass: solaxEvc, ActionType: power, ID: {32bfd377-24d8-4b34-b16a-d13f69c288de})
+----------
+The name of the StateType ({32bfd377-24d8-4b34-b16a-d13f69c288de}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="65"/>
+        <source>Charging time</source>
+        <extracomment>The name of the StateType ({0ca25649-053c-40a8-9353-fd4afa0f9a98}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="68"/>
+        <source>Connected</source>
+        <extracomment>The name of the StateType ({5055c7a8-9b34-4289-b313-8c78c935263d}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="71"/>
+        <source>Connected phases</source>
+        <extracomment>The name of the StateType ({29f2f2e5-3b47-4737-9324-c852c6377037}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="74"/>
+        <source>Current Phase A</source>
+        <extracomment>The name of the StateType ({7234b907-0c10-437e-9824-a8ab706bd3b3}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="77"/>
+        <source>Current Phase B</source>
+        <extracomment>The name of the StateType ({3dd1efb6-92d7-4a34-856f-c6a20581af56}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="80"/>
+        <source>Current Phase C</source>
+        <extracomment>The name of the StateType ({23a369af-7fb5-4e51-bfdd-c0cae2bee841}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="83"/>
+        <source>Fault code</source>
+        <extracomment>The name of the StateType ({38d022d5-c25c-41d4-bec0-d3701e66418f}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="86"/>
+        <source>Firmware version</source>
+        <extracomment>The name of the StateType ({c94d9040-f240-4fc0-90fa-46dae726f42f}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="89"/>
+        <source>IP address</source>
+        <extracomment>The name of the ParamType (ThingClass: solaxEvc, Type: thing, ID: {45a120be-443e-42ee-a97f-dea0b6fbd4d9})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="92"/>
+        <source>MAC address</source>
+        <extracomment>The name of the ParamType (ThingClass: solaxEvc, Type: thing, ID: {1b10e0b6-715d-483a-9a87-cd7a97a525a4})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="95"/>
+        <location filename="../plugininfo.h" line="98"/>
+        <source>Maximum charging current</source>
+        <extracomment>The name of the ParamType (ThingClass: solaxEvc, ActionType: maxChargingCurrent, ID: {2eed9fdc-6ca3-4895-866d-76f503beb75a})
+----------
+The name of the StateType ({2eed9fdc-6ca3-4895-866d-76f503beb75a}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="101"/>
+        <source>Modbus ID</source>
+        <extracomment>The name of the ParamType (ThingClass: solaxEvc, Type: thing, ID: {6e2de5d1-a187-408d-a276-78576ed5fec7})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="104"/>
+        <source>Plugged in</source>
+        <extracomment>The name of the StateType ({819b19e9-3395-472d-95f0-d5c28897eccd}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="107"/>
+        <source>Port</source>
+        <extracomment>The name of the ParamType (ThingClass: solaxEvc, Type: thing, ID: {c5f97cb4-6859-4a3a-b715-eeea81c72fa1})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="110"/>
+        <source>Serial number</source>
+        <extracomment>The name of the StateType ({44feb2a5-3510-4bdc-a3d1-5d4fba38cee0}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="113"/>
+        <source>Session energy</source>
+        <extracomment>The name of the StateType ({c81ca9b1-e9fa-4307-89d9-e3bbfb5262e9}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="116"/>
+        <source>Set charging enabled</source>
+        <extracomment>The name of the ActionType ({32bfd377-24d8-4b34-b16a-d13f69c288de}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="119"/>
+        <source>Set maximum charging current</source>
+        <extracomment>The name of the ActionType ({2eed9fdc-6ca3-4895-866d-76f503beb75a}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="122"/>
+        <location filename="../plugininfo.h" line="125"/>
+        <location filename="../plugininfo.h" line="128"/>
+        <source>Solax EVC</source>
+        <extracomment>The name of the ThingClass ({2ed6595f-f3b9-47e8-ad0e-41b404aa1a36})
+----------
+The name of the vendor ({b0115ec5-8ffa-40ab-98ab-bfc97ae5449d})
+----------
+The name of the plugin solaxEvc ({cd3707ed-d79f-4aba-a5cc-bff1f2d90329})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="131"/>
+        <source>State</source>
+        <extracomment>The name of the StateType ({ce31d3da-5d1a-401a-9fb1-2a93cd9aa7b9}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="134"/>
+        <source>Total consumed energy</source>
+        <extracomment>The name of the StateType ({e9d9e623-0ba0-4418-9271-747e31989438}) of ThingClass solaxEvc</extracomment>
+        <translation></translation>
+    </message>
+</context>
+</TS>
