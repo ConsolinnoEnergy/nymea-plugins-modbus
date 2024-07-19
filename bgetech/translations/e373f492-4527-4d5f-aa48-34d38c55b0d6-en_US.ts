aa67e2bc7c133ffd823626476b2fc1d311ca7d67
Luis Reber <l.reber@consolinno.de>
2024-07-19 11:28:17 +0200

    Updated translations

--- bgetech/translations/e373f492-4527-4d5f-aa48-34d38c55b0d6-en_US.ts
+++ bgetech/translations/e373f492-4527-4d5f-aa48-34d38c55b0d6-en_US.ts
@@ -10,19 +10,19 @@
     </message>
     <message>
         <location filename="../integrationpluginbgetech.cpp" line="75"/>
-        <location filename="../integrationpluginbgetech.cpp" line="97"/>
+        <location filename="../integrationpluginbgetech.cpp" line="98"/>
         <source>The Modbus slave address must be a value between 1 and 254.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../integrationpluginbgetech.cpp" line="128"/>
-        <location filename="../integrationpluginbgetech.cpp" line="246"/>
+        <location filename="../integrationpluginbgetech.cpp" line="130"/>
+        <location filename="../integrationpluginbgetech.cpp" line="248"/>
         <source>The Modbus address not valid. It must be a value between 1 and 254.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../integrationpluginbgetech.cpp" line="135"/>
-        <location filename="../integrationpluginbgetech.cpp" line="253"/>
+        <location filename="../integrationpluginbgetech.cpp" line="137"/>
+        <location filename="../integrationpluginbgetech.cpp" line="255"/>
         <source>The Modbus RTU interface not available.</source>
         <translation></translation>
     </message>
@@ -30,8 +30,8 @@
 <context>
     <name>bgeTech</name>
     <message>
-        <location filename="../plugininfo.h" line="71"/>
-        <location filename="../plugininfo.h" line="74"/>
+        <location filename="../plugininfo.h" line="69"/>
+        <location filename="../plugininfo.h" line="72"/>
         <source>B+G E-Tech</source>
         <extracomment>The name of the vendor ({215035fe-95e8-43d8-a52e-0a31b787d902})
 ----------
@@ -39,8 +39,8 @@ The name of the plugin bgeTech ({e373f492-4527-4d5f-aa48-34d38c55b0d6})</extraco
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="77"/>
-        <location filename="../plugininfo.h" line="80"/>
+        <location filename="../plugininfo.h" line="75"/>
+        <location filename="../plugininfo.h" line="78"/>
         <source>Connected</source>
         <extracomment>The name of the StateType ({3b69f8e9-1788-4b71-8bd6-bece89f1d045}) of ThingClass sdm72
 ----------
@@ -48,8 +48,8 @@ The name of the StateType ({8050bd0b-1dad-4a7e-b632-c71ead3c9f8b}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="83"/>
-        <location filename="../plugininfo.h" line="86"/>
+        <location filename="../plugininfo.h" line="81"/>
+        <location filename="../plugininfo.h" line="84"/>
         <source>Current phase A</source>
         <extracomment>The name of the StateType ({154e1c19-34c5-4e82-ab4b-b4476b7deb18}) of ThingClass sdm72
 ----------
@@ -57,8 +57,8 @@ The name of the StateType ({4baf1d08-5ffa-49cf-95ef-9527b0c6f081}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="89"/>
-        <location filename="../plugininfo.h" line="92"/>
+        <location filename="../plugininfo.h" line="87"/>
+        <location filename="../plugininfo.h" line="90"/>
         <source>Current phase B</source>
         <extracomment>The name of the StateType ({210c5ab5-09ab-48e5-89aa-d932aae5215f}) of ThingClass sdm72
 ----------
@@ -66,8 +66,8 @@ The name of the StateType ({99e47d06-0a6a-4bfd-b164-61ecb6ba2818}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="95"/>
-        <location filename="../plugininfo.h" line="98"/>
+        <location filename="../plugininfo.h" line="93"/>
+        <location filename="../plugininfo.h" line="96"/>
         <source>Current phase C</source>
         <extracomment>The name of the StateType ({abcfd3ee-6065-421d-bcfc-8c5087aacb46}) of ThingClass sdm72
 ----------
@@ -75,8 +75,8 @@ The name of the StateType ({4a092a66-352d-4d60-90ab-6ac5f58b92fe}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="101"/>
-        <location filename="../plugininfo.h" line="104"/>
+        <location filename="../plugininfo.h" line="99"/>
+        <location filename="../plugininfo.h" line="102"/>
         <source>Current power</source>
         <extracomment>The name of the StateType ({d53b97fd-217a-47b6-a73b-f963ece553b2}) of ThingClass sdm72
 ----------
@@ -84,8 +84,8 @@ The name of the StateType ({c824e97b-a6d1-4030-9d7a-00af6fb8e1c3}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="107"/>
-        <location filename="../plugininfo.h" line="110"/>
+        <location filename="../plugininfo.h" line="105"/>
+        <location filename="../plugininfo.h" line="108"/>
         <source>Current power phase A</source>
         <extracomment>The name of the StateType ({c0d7071c-22f7-4ba7-8158-b297fd8a0929}) of ThingClass sdm72
 ----------
@@ -93,8 +93,8 @@ The name of the StateType ({3982fb12-b179-40f7-9b27-36adb1cadd37}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="113"/>
-        <location filename="../plugininfo.h" line="116"/>
+        <location filename="../plugininfo.h" line="111"/>
+        <location filename="../plugininfo.h" line="114"/>
         <source>Current power phase B</source>
         <extracomment>The name of the StateType ({3dd97cae-a03c-4afc-9366-0c135e8c2443}) of ThingClass sdm72
 ----------
@@ -102,8 +102,8 @@ The name of the StateType ({2a231c58-b095-4037-8394-a730431e70b8}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="119"/>
-        <location filename="../plugininfo.h" line="122"/>
+        <location filename="../plugininfo.h" line="117"/>
+        <location filename="../plugininfo.h" line="120"/>
         <source>Current power phase C</source>
         <extracomment>The name of the StateType ({e4043c04-2410-41ff-acab-65883b7a8569}) of ThingClass sdm72
 ----------
@@ -111,53 +111,44 @@ The name of the StateType ({ee8c4f0c-2b69-4210-9966-1553a592b06d}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="125"/>
-        <location filename="../plugininfo.h" line="128"/>
-        <source>Device Name</source>
-        <extracomment>The name of the ParamType (ThingClass: sdm72, Type: discovery, ID: {69c95e3e-1062-48aa-a48a-f0b6d92506a9})
-----------
-The name of the ParamType (ThingClass: sdm630, Type: discovery, ID: {67e4018c-b724-425a-a8b0-76bd86b6fc22})</extracomment>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="131"/>
+        <location filename="../plugininfo.h" line="123"/>
         <source>Energy consumed phase A</source>
         <extracomment>The name of the StateType ({6ca06c81-fe75-4448-a22f-47c303421440}) of ThingClass sdm630</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="134"/>
+        <location filename="../plugininfo.h" line="126"/>
         <source>Energy consumed phase B</source>
         <extracomment>The name of the StateType ({fa2b879b-2a81-4bc8-9577-98082c4d9330}) of ThingClass sdm630</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="137"/>
+        <location filename="../plugininfo.h" line="129"/>
         <source>Energy consumed phase C</source>
         <extracomment>The name of the StateType ({4c084c9e-7a5d-42d1-96b2-a8a4b4a25713}) of ThingClass sdm630</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="140"/>
+        <location filename="../plugininfo.h" line="132"/>
         <source>Energy produced phase A</source>
         <extracomment>The name of the StateType ({308fa88e-6054-4c79-b12a-be2d0a404ef6}) of ThingClass sdm630</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="143"/>
+        <location filename="../plugininfo.h" line="135"/>
         <source>Energy produced phase B</source>
         <extracomment>The name of the StateType ({48ab6e61-dfb4-4f85-b5cc-9d89e53c6b39}) of ThingClass sdm630</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="146"/>
+        <location filename="../plugininfo.h" line="138"/>
         <source>Energy produced phase C</source>
         <extracomment>The name of the StateType ({6b3ddf15-3d4b-4dc1-8e5a-84fbf90b49ff}) of ThingClass sdm630</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="149"/>
-        <location filename="../plugininfo.h" line="152"/>
+        <location filename="../plugininfo.h" line="141"/>
+        <location filename="../plugininfo.h" line="144"/>
         <source>Frequency</source>
         <extracomment>The name of the StateType ({724821e6-1975-4a38-907a-a853b10480bd}) of ThingClass sdm72
 ----------
@@ -165,8 +156,8 @@ The name of the StateType ({ab24f26c-dc15-4ec3-8d76-06a48285440b}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="155"/>
-        <location filename="../plugininfo.h" line="158"/>
+        <location filename="../plugininfo.h" line="147"/>
+        <location filename="../plugininfo.h" line="150"/>
         <source>Modbus RTU master</source>
         <extracomment>The name of the ParamType (ThingClass: sdm72, Type: thing, ID: {6edc9c2b-e939-4c24-ac81-fc3ca17aab3f})
 ----------
@@ -174,8 +165,8 @@ The name of the ParamType (ThingClass: sdm630, Type: thing, ID: {d90e9292-d03c-4
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="161"/>
-        <location filename="../plugininfo.h" line="164"/>
+        <location filename="../plugininfo.h" line="153"/>
+        <location filename="../plugininfo.h" line="156"/>
         <source>Modbus slave address</source>
         <extracomment>The name of the ParamType (ThingClass: sdm72, Type: thing, ID: {6ec32145-f77c-4268-ae1d-159f9702fd85})
 ----------
@@ -183,20 +174,20 @@ The name of the ParamType (ThingClass: sdm630, Type: thing, ID: {ac77ea98-b006-4
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="167"/>
+        <location filename="../plugininfo.h" line="159"/>
         <source>SDM630</source>
         <extracomment>The name of the ThingClass ({f37597bb-35fe-48f2-9617-343dd54c0903})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="170"/>
+        <location filename="../plugininfo.h" line="162"/>
         <source>SDM72</source>
         <extracomment>The name of the ThingClass ({ce54ada6-cbe8-406a-9262-c707c4d8c392})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="173"/>
-        <location filename="../plugininfo.h" line="176"/>
+        <location filename="../plugininfo.h" line="165"/>
+        <location filename="../plugininfo.h" line="168"/>
         <source>Slave address</source>
         <extracomment>The name of the ParamType (ThingClass: sdm72, Type: discovery, ID: {27219539-443b-43dc-ad8b-11e7cfb862b5})
 ----------
@@ -204,8 +195,8 @@ The name of the ParamType (ThingClass: sdm630, Type: discovery, ID: {6ab43559-53
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="179"/>
-        <location filename="../plugininfo.h" line="182"/>
+        <location filename="../plugininfo.h" line="171"/>
+        <location filename="../plugininfo.h" line="174"/>
         <source>Total energy consumed</source>
         <extracomment>The name of the StateType ({8a9a9212-21aa-4d5a-a528-7781a087f3a2}) of ThingClass sdm72
 ----------
@@ -213,8 +204,8 @@ The name of the StateType ({98d858a8-22e8-4262-b5c7-25bb027942ad}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="185"/>
-        <location filename="../plugininfo.h" line="188"/>
+        <location filename="../plugininfo.h" line="177"/>
+        <location filename="../plugininfo.h" line="180"/>
         <source>Total energy produced</source>
         <extracomment>The name of the StateType ({7b8592ca-2173-459e-85ab-9b4aec4970c6}) of ThingClass sdm72
 ----------
@@ -222,8 +213,8 @@ The name of the StateType ({e469b3ff-a4c2-42da-af35-ccafaef214af}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="191"/>
-        <location filename="../plugininfo.h" line="194"/>
+        <location filename="../plugininfo.h" line="183"/>
+        <location filename="../plugininfo.h" line="186"/>
         <source>Voltage phase A</source>
         <extracomment>The name of the StateType ({15b5e57b-7584-4be0-9053-55bb495435a5}) of ThingClass sdm72
 ----------
@@ -231,8 +222,8 @@ The name of the StateType ({db018146-0441-4dc0-9834-6d43ebaf8311}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="197"/>
-        <location filename="../plugininfo.h" line="200"/>
+        <location filename="../plugininfo.h" line="189"/>
+        <location filename="../plugininfo.h" line="192"/>
         <source>Voltage phase B</source>
         <extracomment>The name of the StateType ({998cf8e7-38e9-433a-8ef5-726b45bbfc63}) of ThingClass sdm72
 ----------
@@ -240,8 +231,8 @@ The name of the StateType ({406f6d02-d5eb-49b3-87da-3247568e6054}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="203"/>
-        <location filename="../plugininfo.h" line="206"/>
+        <location filename="../plugininfo.h" line="195"/>
+        <location filename="../plugininfo.h" line="198"/>
         <source>Voltage phase C</source>
         <extracomment>The name of the StateType ({48164e10-0217-45e2-bf8f-5ec396c43ceb}) of ThingClass sdm72
 ----------
