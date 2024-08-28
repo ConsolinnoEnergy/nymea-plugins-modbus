0c79e9cf3bd6351baa0001d8d5930e6f7162440a
Luis Reber <l.reber@consolinno.de>
2024-08-28 10:17:52 +0200

    Updated translations

--- qcellswb/translations/efc090ee-4230-4755-ad87-fb2675ca0186-de.ts
+++ qcellswb/translations/efc090ee-4230-4755-ad87-fb2675ca0186-de.ts
@@ -2,189 +2,189 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
-    <name>IntegrationPluginFoxEss</name>
+    <name>IntegrationPluginQCells</name>
     <message>
-        <location filename="../integrationpluginfoxesswbv3.cpp" line="65"/>
+        <location filename="../integrationpluginqcellswb.cpp" line="69"/>
         <source>The wallbox was found, but the MAC address is invalid. Try searching again.</source>
         <translation>Die Wallbox wurde gefunden, aber die MAC Adresse ist ungültig. Bitte versuchen Sie es erneut.</translation>
     </message>
     <message>
-        <location filename="../integrationpluginfoxesswbv3.cpp" line="104"/>
+        <location filename="../integrationpluginqcellswb.cpp" line="108"/>
         <source>The MAC address is not vaild. Please reconfigure the device to fix this.</source>
         <translation>Die MAC Adresse ist ungültig. Bitte konfigurieren Sie das Gerät neu.</translation>
     </message>
 </context>
 <context>
-    <name>foxEss</name>
+    <name>qcells</name>
     <message>
         <location filename="../plugininfo.h" line="55"/>
         <source>Active power</source>
-        <extracomment>The name of the StateType ({56b2bcdd-5275-46d3-adf6-5e543d002119}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({c7bf22ef-7a73-463b-bfb9-f386356d4c12}) of ThingClass qCells</extracomment>
         <translation>Aktuelle Leistung</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="58"/>
         <source>Alarm info</source>
-        <extracomment>The name of the StateType ({59afc8c9-11a8-4f97-a03d-390710dbafc8}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({efb64f14-ce09-4b0b-ba51-b515dea5a8e7}) of ThingClass qCells</extracomment>
         <translation>Alarm Info</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="61"/>
         <source>Charging</source>
-        <extracomment>The name of the StateType ({d9583872-6a1d-4295-b394-d72690e4acb6}) of ThingClass foxEss</extracomment>
-        <translation>Lädt</translation>
+        <extracomment>The name of the StateType ({bb796723-de92-413b-9297-db9a26e3511d}) of ThingClass qCells</extracomment>
+        <translation>Laden aktiv</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="64"/>
         <location filename="../plugininfo.h" line="67"/>
         <source>Charging enabled</source>
-        <extracomment>The name of the ParamType (ThingClass: foxEss, ActionType: power, ID: {a5f90367-7eea-4786-9404-e68d120769d0})
+        <extracomment>The name of the ParamType (ThingClass: qCells, ActionType: power, ID: {9607d22b-a2de-45d8-9745-3b56e141eae7})
 ----------
-The name of the StateType ({a5f90367-7eea-4786-9404-e68d120769d0}) of ThingClass foxEss</extracomment>
-        <translation>Laden aktiv</translation>
+The name of the StateType ({9607d22b-a2de-45d8-9745-3b56e141eae7}) of ThingClass qCells</extracomment>
+        <translation>Laden aktiviert</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="70"/>
         <source>Connected</source>
-        <extracomment>The name of the StateType ({ec59d425-e998-4810-9aca-a68ce2ae6208}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({656043d8-8f6f-4fec-a4f8-ef94495d8082}) of ThingClass qCells</extracomment>
         <translation>Verbunden</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="73"/>
         <source>Connected phases</source>
-        <extracomment>The name of the StateType ({989e9a6d-9af2-4926-b2dc-5ec6569751e8}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({1c58571c-75bd-4747-a914-4b7c5754e601}) of ThingClass qCells</extracomment>
         <translation>Phasenanhzahl</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="76"/>
         <source>Current Phase A</source>
-        <extracomment>The name of the StateType ({9e4344a2-abbd-4912-a14e-a1ac6c8edba0}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({50499ffc-9612-4631-8342-994247dfa165}) of ThingClass qCells</extracomment>
         <translation>Strom Phase A</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="79"/>
         <source>Current Phase B</source>
-        <extracomment>The name of the StateType ({3fa81e2b-6bc0-4091-b799-fbd6e464eec4}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({9206413e-8a23-48eb-97e8-93487c292bbb}) of ThingClass qCells</extracomment>
         <translation>Strom Phase B</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="82"/>
         <source>Current Phase C</source>
-        <extracomment>The name of the StateType ({3b925f1c-40e6-4221-9ad8-28504e48ceb0}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({5cf9c779-b222-4a3c-9e8a-de74d1a62d98}) of ThingClass qCells</extracomment>
         <translation>Strom Phase C</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="85"/>
         <source>Fault info</source>
-        <extracomment>The name of the StateType ({0295789c-95da-4951-8b64-22f4e3ee9d6f}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({d25f73be-bb5f-42ed-b14f-22011bb24144}) of ThingClass qCells</extracomment>
         <translation>Fehler Info</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="88"/>
         <source>Firmware version</source>
-        <extracomment>The name of the StateType ({496dfb8a-a9a4-4fb9-bb48-5d0089f1e864}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({74fb2562-fa64-40a8-9f8e-83b5a76b5ece}) of ThingClass qCells</extracomment>
         <translation>Firmware Version</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="91"/>
-        <location filename="../plugininfo.h" line="94"/>
-        <source>FoxESS</source>
-        <extracomment>The name of the vendor ({17e4357f-c6a6-4407-857c-354e813c4c92})
-----------
-The name of the plugin foxEss ({efc090ee-4230-4755-ad87-fb2675ca0186})</extracomment>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="97"/>
-        <source>FoxESS Wallbox 3rd Gen</source>
-        <extracomment>The name of the ThingClass ({2e77930f-bd60-44ff-96b0-05914a352fd1})</extracomment>
-        <translation>FoxESS Wallbox 3. Gen</translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="100"/>
         <source>IP address</source>
-        <extracomment>The name of the ParamType (ThingClass: foxEss, Type: thing, ID: {8a515563-69ac-4d64-8eac-83bf2168e017})</extracomment>
+        <extracomment>The name of the ParamType (ThingClass: qCells, Type: thing, ID: {afc735b7-94f8-41ef-8a02-79a6e8e6856c})</extracomment>
         <translation>IP Adresse</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="103"/>
+        <location filename="../plugininfo.h" line="94"/>
         <source>MAC address</source>
-        <extracomment>The name of the ParamType (ThingClass: foxEss, Type: thing, ID: {f0541347-81a9-4e70-b423-e70443a86465})</extracomment>
+        <extracomment>The name of the ParamType (ThingClass: qCells, Type: thing, ID: {4527655a-8461-48a4-a284-ec3fe9788639})</extracomment>
         <translation>MAC Adresse</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="106"/>
-        <location filename="../plugininfo.h" line="109"/>
+        <location filename="../plugininfo.h" line="97"/>
+        <location filename="../plugininfo.h" line="100"/>
         <source>Maximum charging current</source>
-        <extracomment>The name of the ParamType (ThingClass: foxEss, ActionType: maxChargingCurrent, ID: {7423ff43-d495-4ce3-b785-a4af5b5240f0})
+        <extracomment>The name of the ParamType (ThingClass: qCells, ActionType: maxChargingCurrent, ID: {c8906960-9051-4944-9d22-cecd72dfb5e3})
 ----------
-The name of the StateType ({7423ff43-d495-4ce3-b785-a4af5b5240f0}) of ThingClass foxEss</extracomment>
+The name of the StateType ({c8906960-9051-4944-9d22-cecd72dfb5e3}) of ThingClass qCells</extracomment>
         <translation>Maximaler Ladestrom</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="112"/>
+        <location filename="../plugininfo.h" line="103"/>
         <source>Modbus ID</source>
-        <extracomment>The name of the ParamType (ThingClass: foxEss, Type: thing, ID: {07d92e5c-8cf7-461d-a6c6-df50dfc0f769})</extracomment>
+        <extracomment>The name of the ParamType (ThingClass: qCells, Type: thing, ID: {b3cc0ee4-e790-46c0-be25-1aa35daab7f5})</extracomment>
         <translation>Modbus ID</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="115"/>
+        <location filename="../plugininfo.h" line="106"/>
         <source>Plugged in</source>
-        <extracomment>The name of the StateType ({04983650-82db-4e28-85ca-7011249acd8e}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({f480b04e-c268-4b3e-b69e-b811e66dbcc8}) of ThingClass qCells</extracomment>
         <translation>Eingesteckt</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="118"/>
+        <location filename="../plugininfo.h" line="109"/>
         <source>Port</source>
-        <extracomment>The name of the ParamType (ThingClass: foxEss, Type: thing, ID: {c463e362-8623-4ba3-9340-e9bd71e4adf3})</extracomment>
+        <extracomment>The name of the ParamType (ThingClass: qCells, Type: thing, ID: {f3f5c8a7-5e24-4370-98f4-275566f39d64})</extracomment>
         <translation>Port</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="112"/>
+        <location filename="../plugininfo.h" line="115"/>
+        <source>QCells</source>
+        <extracomment>The name of the vendor ({f6e8922c-89ce-4971-b5e2-c3385fe0494f})
+----------
+The name of the plugin qcells ({32f5d13d-c372-466c-b166-3379682c51b6})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="118"/>
+        <source>QCells Wallbox</source>
+        <extracomment>The name of the ThingClass ({12137d3d-45de-4594-b046-9e7dd46a0dcb})</extracomment>
+        <translation></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="121"/>
         <source>Serial number</source>
-        <extracomment>The name of the StateType ({51c493b6-6c3e-4257-94e2-253d5bdd48ca}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({2373348e-414f-4a46-933a-907ec6c2aad5}) of ThingClass qCells</extracomment>
         <translation>Seriennummer</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="124"/>
         <source>Session energy</source>
-        <extracomment>The name of the StateType ({606871b9-329d-4a15-b12e-9623fed13423}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({0d1e9850-6f6c-40cf-b44d-aaa5d3973968}) of ThingClass qCells</extracomment>
         <translation>Aktuell geladene Energie</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="127"/>
         <source>Set charging enabled</source>
-        <extracomment>The name of the ActionType ({a5f90367-7eea-4786-9404-e68d120769d0}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the ActionType ({9607d22b-a2de-45d8-9745-3b56e141eae7}) of ThingClass qCells</extracomment>
         <translation>Laden aktiviert</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="130"/>
         <source>Set maximum charging current</source>
-        <extracomment>The name of the ActionType ({7423ff43-d495-4ce3-b785-a4af5b5240f0}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the ActionType ({c8906960-9051-4944-9d22-cecd72dfb5e3}) of ThingClass qCells</extracomment>
         <translation>Setze maximalen Ladestrom</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="133"/>
         <source>State</source>
-        <extracomment>The name of the StateType ({8868f435-d983-4446-b840-dffaa719a35c}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({47b17931-4aef-4aa1-aab3-d678990dbdb4}) of ThingClass qCells</extracomment>
         <translation>Status</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="136"/>
         <source>Stop Reason</source>
-        <extracomment>The name of the StateType ({51f6560e-dfd5-4a71-8951-d4a1bf0627be}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({4c71dcd0-7b13-450a-993e-66ec8b1e549f}) of ThingClass qCells</extracomment>
         <translation>Stop Grund</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="139"/>
         <source>Total consumed energy</source>
-        <extracomment>The name of the StateType ({318a4261-c084-4ead-8e63-96448ea7a794}) of ThingClass foxEss</extracomment>
+        <extracomment>The name of the StateType ({f031cc71-7b5f-45af-b86a-b593a6613893}) of ThingClass qCells</extracomment>
         <translation>Gesamt geladene Energie</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="142"/>
         <source>mDNS Name</source>
-        <extracomment>The name of the ParamType (ThingClass: foxEss, Type: thing, ID: {30b01da0-0a4e-47a6-a4e0-2b52a87bd702})</extracomment>
+        <extracomment>The name of the ParamType (ThingClass: qCells, Type: thing, ID: {869fd27e-5a0d-48e3-abbd-df0aa14c04a7})</extracomment>
         <translation>mDNS Name</translation>
     </message>
 </context>
