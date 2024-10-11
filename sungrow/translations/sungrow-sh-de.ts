747e5ec8215cb3a3d3e36286e38b8f7f68dfb8e7
Luis Reber <l.reber@consolinno.de>
2024-10-11 10:24:25 +0200

    Updated translations

--- sungrow/translations/sungrow-sh-de.ts
+++ sungrow/translations/sungrow-sh-de.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US">
+<TS version="2.1" language="de_DE">
 <context>
     <name>IntegrationPluginSungrow</name>
     <message>
         <location filename="../integrationpluginsungrow.cpp" line="47"/>
         <source>The network device discovery is not available.</source>
-        <translation></translation>
+        <translation>Die Netzwerkerkennung ist nicht verfügbar.</translation>
     </message>
     <message>
         <location filename="../integrationpluginsungrow.cpp" line="103"/>
         <source>The MAC address is not known. Please reconfigure this inverter.</source>
-        <translation></translation>
+        <translation>The MAC Adresse ist nicht bekannt. Bitte konfigurieren Sie den Wechselrichter neu.</translation>
     </message>
 </context>
 <context>
@@ -20,31 +20,31 @@
         <location filename="../plugininfo.h" line="55"/>
         <source>Active power</source>
         <extracomment>The name of the StateType ({6fb11fe8-da07-11ee-b77f-8393cc11be21}) of ThingClass sungrowInverterTcp</extracomment>
-        <translation></translation>
+        <translation>Aktive Leistung</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="58"/>
         <source>Battery critical</source>
         <extracomment>The name of the StateType ({13f48cde-da08-11ee-81c8-3362a92b58c8}) of ThingClass sungrowBattery</extracomment>
-        <translation></translation>
+        <translation>Batterie kritisch</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="61"/>
         <source>Battery level</source>
         <extracomment>The name of the StateType ({1738bcf8-da08-11ee-b1d0-a3e1183dabba}) of ThingClass sungrowBattery</extracomment>
-        <translation></translation>
+        <translation>Batterstand</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="64"/>
         <source>Capacity</source>
         <extracomment>The name of the StateType ({264ac092-da08-11ee-ad8d-9f0751d6c499}) of ThingClass sungrowBattery</extracomment>
-        <translation></translation>
+        <translation>Kapazität</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="67"/>
         <source>Charging state</source>
         <extracomment>The name of the StateType ({29e8a6d8-da08-11ee-87ba-539307b7d2ee}) of ThingClass sungrowBattery</extracomment>
-        <translation></translation>
+        <translation>Ladezustand</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="70"/>
@@ -56,31 +56,31 @@
 The name of the StateType ({d26b59dc-da07-11ee-b494-3781cc3081a7}) of ThingClass sungrowMeter
 ----------
 The name of the StateType ({6c68b170-da07-11ee-9891-3335ced0ad72}) of ThingClass sungrowInverterTcp</extracomment>
-        <translation></translation>
+        <translation>Verbunden</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="79"/>
         <source>Current phase A</source>
         <extracomment>The name of the StateType ({f3d850c0-da07-11ee-883f-4ff8b7e55bdc}) of ThingClass sungrowMeter</extracomment>
-        <translation></translation>
+        <translation>Strom Phase A</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="82"/>
         <source>Current phase B</source>
         <extracomment>The name of the StateType ({f82603de-da07-11ee-93bc-6b9f9f333c30}) of ThingClass sungrowMeter</extracomment>
-        <translation></translation>
+        <translation>Strom Phase B</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="85"/>
         <source>Current phase C</source>
         <extracomment>The name of the StateType ({fcf77988-da07-11ee-99da-3b2415014506}) of ThingClass sungrowMeter</extracomment>
-        <translation></translation>
+        <translation>Strom Phase C</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="88"/>
         <source>Current power</source>
         <extracomment>The name of the StateType ({d777530e-da07-11ee-a526-c7d23dd34f57}) of ThingClass sungrowMeter</extracomment>
-        <translation></translation>
+        <translation>Aktuelle Leistung</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="91"/>
@@ -89,13 +89,13 @@ The name of the StateType ({6c68b170-da07-11ee-9891-3335ced0ad72}) of ThingClass
         <extracomment>The name of the StateType ({07b526ea-da08-11ee-ab24-ab0d1ca8555d}) of ThingClass sungrowMeter
 ----------
 The name of the StateType ({84bcbc58-da07-11ee-b94d-a39792ee6f59}) of ThingClass sungrowInverterTcp</extracomment>
-        <translation></translation>
+        <translation>Frequenz</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="97"/>
         <source>MAC address</source>
         <extracomment>The name of the ParamType (ThingClass: sungrowInverterTcp, Type: thing, ID: {62137142-da07-11ee-9522-2f74f3b1fc5d})</extracomment>
-        <translation></translation>
+        <translation>MAC Adresse</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="100"/>
@@ -110,13 +110,13 @@ The name of the plugin Sungrow ({250c9b83-1127-4013-bbd0-11e7ea482057})</extraco
         <location filename="../plugininfo.h" line="106"/>
         <source>Sungrow Battery</source>
         <extracomment>The name of the ThingClass ({0aea1b90-da08-11ee-9195-afc9f857a324})</extracomment>
-        <translation></translation>
+        <translation>Sungrow Batterie</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="109"/>
         <source>Sungrow Meter</source>
         <extracomment>The name of the ThingClass ({a935e49c-da07-11ee-bd11-3fcf27dc6373})</extracomment>
-        <translation></translation>
+        <translation>Sungrow Zähler</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="112"/>
@@ -131,55 +131,55 @@ The name of the plugin Sungrow ({250c9b83-1127-4013-bbd0-11e7ea482057})</extraco
         <extracomment>The name of the StateType ({221ccae2-da08-11ee-9e4d-4b6abbf9e564}) of ThingClass sungrowBattery
 ----------
 The name of the StateType ({80b35fc2-da07-11ee-a73d-5774b082c92b}) of ThingClass sungrowInverterTcp</extracomment>
-        <translation></translation>
+        <translation>Temperatur</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="121"/>
         <source>Total energy produced</source>
         <extracomment>The name of the StateType ({7bf092a2-da07-11ee-abe9-138e66e6f2a5}) of ThingClass sungrowInverterTcp</extracomment>
-        <translation></translation>
+        <translation>Gesamt produzierte Energie</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="124"/>
         <source>Total imported energy</source>
         <extracomment>The name of the StateType ({03ef972a-da08-11ee-9a1f-d741d1e276be}) of ThingClass sungrowMeter</extracomment>
-        <translation></translation>
+        <translation>Gesamt importierte Energie</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="127"/>
         <source>Total real power</source>
         <extracomment>The name of the StateType ({1aa8cf86-da08-11ee-9697-3b0a8023db88}) of ThingClass sungrowBattery</extracomment>
-        <translation></translation>
+        <translation>Reale Leistung</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="130"/>
         <source>Total returned energy</source>
         <extracomment>The name of the StateType ({00eb83c2-da08-11ee-b67d-1f74a41e6218}) of ThingClass sungrowMeter</extracomment>
-        <translation></translation>
+        <translation>Gesamt eingespeiste Leistung</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="133"/>
         <source>Voltage</source>
         <extracomment>The name of the StateType ({1e6bf760-da08-11ee-ba39-c31ecdbb8fc9}) of ThingClass sungrowBattery</extracomment>
-        <translation></translation>
+        <translation>Spannung</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="136"/>
         <source>Voltage phase A</source>
         <extracomment>The name of the StateType ({e80bb836-da07-11ee-afd1-dbff8484ba11}) of ThingClass sungrowMeter</extracomment>
-        <translation></translation>
+        <translation>Spannung Phase A</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="139"/>
         <source>Voltage phase B</source>
         <extracomment>The name of the StateType ({ebefd252-da07-11ee-a8a4-c7ea9df0b6f9}) of ThingClass sungrowMeter</extracomment>
-        <translation></translation>
+        <translation>Spannung Phase B</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="142"/>
         <source>Voltage phase C</source>
         <extracomment>The name of the StateType ({efd82b30-da07-11ee-9668-7b523696940d}) of ThingClass sungrowMeter</extracomment>
-        <translation></translation>
+        <translation>Spannung Phase C</translation>
     </message>
 </context>
 </TS>
