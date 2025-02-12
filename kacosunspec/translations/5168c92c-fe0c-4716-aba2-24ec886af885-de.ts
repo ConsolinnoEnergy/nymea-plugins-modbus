18a270d48f58c3ae43505863447aeea88debbcfe
Luis Reber <l.reber@consolinno.de>
2025-02-12 14:52:22 +0100

    Updated kaco translation

--- kacosunspec/translations/5168c92c-fe0c-4716-aba2-24ec886af885-de.ts
+++ kacosunspec/translations/5168c92c-fe0c-4716-aba2-24ec886af885-de.ts
@@ -44,9 +44,9 @@
 <context>
     <name>KacoSunSpec</name>
     <message>
-        <location filename="../plugininfo.h" line="77"/>
-        <location filename="../plugininfo.h" line="80"/>
-        <location filename="../plugininfo.h" line="83"/>
+        <location filename="../plugininfo.h" line="75"/>
+        <location filename="../plugininfo.h" line="78"/>
+        <location filename="../plugininfo.h" line="81"/>
         <source>Active power</source>
         <extracomment>The name of the StateType ({da916026-ae5b-47a0-82f8-0579bfc4c85b}) of ThingClass kaconh3
 ----------
@@ -56,41 +56,41 @@ The name of the StateType ({bb8cdb1e-3612-42b4-a995-2b3179cbb741}) of ThingClass
         <translation>Aktuelle Leistung</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="86"/>
+        <location filename="../plugininfo.h" line="84"/>
         <source>Battery State</source>
         <extracomment>The name of the StateType ({e37edd3e-9696-4f79-9566-36b793566e30}) of ThingClass kacoNh3Battery</extracomment>
         <translation>Batterie Status</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="89"/>
+        <location filename="../plugininfo.h" line="87"/>
         <source>Battery critical</source>
         <extracomment>The name of the StateType ({246bb394-1c92-4f12-873e-4a5cf69ab2b6}) of ThingClass kacoNh3Battery</extracomment>
         <translation>Batterie kritisch</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="92"/>
+        <location filename="../plugininfo.h" line="90"/>
         <source>Battery level</source>
         <extracomment>The name of the StateType ({aef83cc1-6b3e-416c-8326-73fd3cc0bfc0}) of ThingClass kacoNh3Battery</extracomment>
         <translation>Batteriestand</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="95"/>
+        <location filename="../plugininfo.h" line="93"/>
         <source>Capacity</source>
         <extracomment>The name of the StateType ({6b47f8bd-e1e7-4b46-9fc2-73b80a567627}) of ThingClass kacoNh3Battery</extracomment>
         <translation>Kapazität</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="98"/>
+        <location filename="../plugininfo.h" line="96"/>
         <source>Charging state</source>
         <extracomment>The name of the StateType ({e167d7fb-d350-4434-93e3-859f67957477}) of ThingClass kacoNh3Battery</extracomment>
         <translation>Ladestatus</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="101"/>
-        <location filename="../plugininfo.h" line="104"/>
-        <location filename="../plugininfo.h" line="107"/>
-        <location filename="../plugininfo.h" line="110"/>
-        <location filename="../plugininfo.h" line="113"/>
+        <location filename="../plugininfo.h" line="99"/>
+        <location filename="../plugininfo.h" line="102"/>
+        <location filename="../plugininfo.h" line="105"/>
+        <location filename="../plugininfo.h" line="108"/>
+        <location filename="../plugininfo.h" line="111"/>
         <source>Connected</source>
         <extracomment>The name of the StateType ({5c81631c-bd41-4064-91f7-279468f12d8f}) of ThingClass kacoNh3Battery
 ----------
@@ -104,92 +104,92 @@ The name of the StateType ({dd6739c5-c7c9-4ebf-902b-0f3b2cbaa0f6}) of ThingClass
         <translation>Verbunden</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="116"/>
+        <location filename="../plugininfo.h" line="114"/>
         <source>Current phase A</source>
         <extracomment>The name of the StateType ({72d35ae8-59e4-4f5e-9a2f-0b638196f3ea}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Strom Phase A</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="119"/>
+        <location filename="../plugininfo.h" line="117"/>
         <source>Current phase B</source>
         <extracomment>The name of the StateType ({8fcec532-8947-4b67-96d6-8c6ed9983691}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Strom Phase B</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="122"/>
+        <location filename="../plugininfo.h" line="120"/>
         <source>Current phase C</source>
         <extracomment>The name of the StateType ({a1629103-86f5-46d5-a700-aedb208a7748}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Strom Phase C</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="125"/>
+        <location filename="../plugininfo.h" line="123"/>
         <source>Current power</source>
         <extracomment>The name of the StateType ({9bd06abc-1742-446b-9668-7dc58af444e0}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Aktuelle Leistung</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="128"/>
+        <location filename="../plugininfo.h" line="126"/>
         <source>Device Model</source>
         <extracomment>The name of the StateType ({48155b69-c3a1-4983-b6f6-930a2c27f02e}) of ThingClass kaconh3</extracomment>
         <translation>Gerätemodell</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="131"/>
+        <location filename="../plugininfo.h" line="129"/>
         <source>Firmware Version</source>
         <extracomment>The name of the StateType ({2201baf0-e7b0-4adc-9ef5-bcacaab8f90a}) of ThingClass kaconh3</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="134"/>
+        <location filename="../plugininfo.h" line="132"/>
         <source>Frequency</source>
         <extracomment>The name of the StateType ({f5f882e7-34cf-4ae4-a353-edf77099b2b9}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Frequenz</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="137"/>
+        <location filename="../plugininfo.h" line="135"/>
         <source>Kaco Battery</source>
         <extracomment>The name of the ThingClass ({9b5558cb-e154-4bd4-8ebc-cfaa67e4b22a})</extracomment>
         <translation>Kaco Batterie</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="140"/>
+        <location filename="../plugininfo.h" line="138"/>
         <source>Kaco Inverter (Modbus RTU)</source>
         <extracomment>The name of the ThingClass ({4fa73aab-377f-41ce-b167-14654f157b88})</extracomment>
         <translation>Kaco Wechselrichter (Modbus RTU)</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="143"/>
+        <location filename="../plugininfo.h" line="141"/>
         <source>Kaco Inverter (Modbus TCP)</source>
         <extracomment>The name of the ThingClass ({2c6f9d45-44a2-4a16-96d8-76be4162ffcf})</extracomment>
         <translation>Kaco Wechselrichter (Modbus TCP)</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="146"/>
+        <location filename="../plugininfo.h" line="144"/>
         <source>Kaco Meter</source>
         <extracomment>The name of the ThingClass ({efae220a-6f55-4466-afe8-cea2b94e0c49})</extracomment>
         <translation>Kaco Zähler</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="149"/>
+        <location filename="../plugininfo.h" line="147"/>
         <source>Kaco NH3</source>
         <extracomment>The name of the ThingClass ({ec4e0f1b-e5db-42f3-9486-06bd61459daa})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="152"/>
+        <location filename="../plugininfo.h" line="150"/>
         <source>Kaco SunSpec</source>
         <extracomment>The name of the plugin KacoSunSpec ({5168c92c-fe0c-4716-aba2-24ec886af885})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="155"/>
+        <location filename="../plugininfo.h" line="153"/>
         <source>Kaco new energy</source>
         <extracomment>The name of the vendor ({41bd21a1-6c34-4a2a-907c-859ce0efc012})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="158"/>
-        <location filename="../plugininfo.h" line="161"/>
+        <location filename="../plugininfo.h" line="156"/>
+        <location filename="../plugininfo.h" line="159"/>
         <source>MAC address</source>
         <extracomment>The name of the ParamType (ThingClass: kaconh3, Type: thing, ID: {cbe847df-fef9-4ccf-b5a3-ad554d0b0746})
 ----------
@@ -197,39 +197,27 @@ The name of the ParamType (ThingClass: kacosunspecInverterTCP, Type: thing, ID: 
         <translation>MAC Adresse</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="164"/>
+        <location filename="../plugininfo.h" line="162"/>
         <source>Max Charge / Discharge Rate</source>
         <extracomment>The name of the StateType ({cd745862-65b3-4459-b9ff-5622970ffccf}) of ThingClass kacoNh3Battery</extracomment>
         <translation>Maximale Auf-/Entladerate</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="167"/>
-        <source>Maximum SoC</source>
-        <extracomment>The name of the StateType ({dbadf3c0-c1b6-49d9-bd51-45a692477718}) of ThingClass kacoNh3Battery</extracomment>
-        <translation>Maximaler SoC</translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="170"/>
-        <source>Minimum SoC</source>
-        <extracomment>The name of the StateType ({cc3d3a71-586e-4f90-ac18-637e39532d45}) of ThingClass kacoNh3Battery</extracomment>
-        <translation>Minimaler SoC</translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="173"/>
+        <location filename="../plugininfo.h" line="165"/>
         <source>Modbus RTU master</source>
         <extracomment>The name of the ParamType (ThingClass: kacosunspecInverterRTU, Type: thing, ID: {8ccaf129-c303-49a3-be4f-d891f0bff988})</extracomment>
         <translation>Modbus RTU Master</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="176"/>
+        <location filename="../plugininfo.h" line="168"/>
         <source>Modbus slave address</source>
         <extracomment>The name of the ParamType (ThingClass: kacosunspecInverterRTU, Type: thing, ID: {c5cb32d8-2811-4610-8f7e-1edb55f58729})</extracomment>
         <translation>Modbus ID</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="179"/>
-        <location filename="../plugininfo.h" line="182"/>
-        <location filename="../plugininfo.h" line="185"/>
+        <location filename="../plugininfo.h" line="171"/>
+        <location filename="../plugininfo.h" line="174"/>
+        <location filename="../plugininfo.h" line="177"/>
         <source>Operating State</source>
         <extracomment>The name of the StateType ({a22cc860-1b5d-4480-a8b4-161169426015}) of ThingClass kaconh3
 ----------
@@ -239,8 +227,8 @@ The name of the StateType ({a7bb1db5-1100-4ddc-acd6-3b9e325d1977}) of ThingClass
         <translation>Betriebszustand</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="188"/>
-        <location filename="../plugininfo.h" line="191"/>
+        <location filename="../plugininfo.h" line="180"/>
+        <location filename="../plugininfo.h" line="183"/>
         <source>Port</source>
         <extracomment>The name of the ParamType (ThingClass: kaconh3, Type: thing, ID: {4b2c4e60-24b1-431e-83be-357013db00d8})
 ----------
@@ -248,14 +236,14 @@ The name of the ParamType (ThingClass: kacosunspecInverterTCP, Type: thing, ID: 
         <translation>Port</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="194"/>
+        <location filename="../plugininfo.h" line="186"/>
         <source>Serial Number</source>
         <extracomment>The name of the StateType ({0635d700-c356-41b3-8932-50ad0a18cfa6}) of ThingClass kaconh3</extracomment>
         <translation>Seriennummer</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="197"/>
-        <location filename="../plugininfo.h" line="200"/>
+        <location filename="../plugininfo.h" line="189"/>
+        <location filename="../plugininfo.h" line="192"/>
         <source>Slave ID</source>
         <extracomment>The name of the ParamType (ThingClass: kaconh3, Type: thing, ID: {fd176c9f-9bcb-4413-871f-611010f70237})
 ----------
@@ -263,15 +251,15 @@ The name of the ParamType (ThingClass: kacosunspecInverterTCP, Type: thing, ID: 
         <translation>Modbus ID</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="203"/>
+        <location filename="../plugininfo.h" line="195"/>
         <source>Slave address</source>
         <extracomment>The name of the ParamType (ThingClass: kacosunspecInverterRTU, Type: discovery, ID: {95f393de-13bc-4e32-b538-0c9f097e4c75})</extracomment>
         <translation>Modbus ID</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="206"/>
-        <location filename="../plugininfo.h" line="209"/>
-        <location filename="../plugininfo.h" line="212"/>
+        <location filename="../plugininfo.h" line="198"/>
+        <location filename="../plugininfo.h" line="201"/>
+        <location filename="../plugininfo.h" line="204"/>
         <source>Total energy produced</source>
         <extracomment>The name of the StateType ({3161fb31-4f1f-4a26-90e8-b0f7fceb2087}) of ThingClass kaconh3
 ----------
@@ -281,37 +269,37 @@ The name of the StateType ({057c00ca-ee8e-4914-b618-e686e2a48906}) of ThingClass
         <translation>Gesamt produzierte Energie</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="215"/>
+        <location filename="../plugininfo.h" line="207"/>
         <source>Total imported energy</source>
         <extracomment>The name of the StateType ({c5f95082-d0a0-4c39-a331-728b253d2139}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Gesamt importierte Energie</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="218"/>
+        <location filename="../plugininfo.h" line="210"/>
         <source>Total real power</source>
         <extracomment>The name of the StateType ({e23c96f2-1105-435a-87de-ea433335511f}) of ThingClass kacoNh3Battery</extracomment>
         <translation>Gesamte reale Leistung</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="221"/>
+        <location filename="../plugininfo.h" line="213"/>
         <source>Total returned energy</source>
         <extracomment>The name of the StateType ({d79eb52a-5892-48c8-bc20-5d1e70e16da5}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Gesamte erzeugte Energie</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="224"/>
+        <location filename="../plugininfo.h" line="216"/>
         <source>Voltage phase A</source>
         <extracomment>The name of the StateType ({2eb6a484-53d3-4315-ac68-66bd3992201c}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Spannung Phase A</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="227"/>
+        <location filename="../plugininfo.h" line="219"/>
         <source>Voltage phase B</source>
         <extracomment>The name of the StateType ({ea780a28-dbde-4579-9cfa-b05b84260e33}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Spannung Phase B</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="230"/>
+        <location filename="../plugininfo.h" line="222"/>
         <source>Voltage phase C</source>
         <extracomment>The name of the StateType ({f1ecf8e5-c54e-4cfa-9583-e2c9f7dfbde2}) of ThingClass kacoNh3Meter</extracomment>
         <translation>Spannung Phase C</translation>
