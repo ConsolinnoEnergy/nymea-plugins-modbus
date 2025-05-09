df9ff4e3eb4077981060901fccb19cd97c34d4fa
Johannes Obermeier <j.obermeier@consolinno.de>
2025-05-09 08:41:08 +0200

    added controllablebattery interface

--- victron/README.md
+++ victron/README.md
@@ -7,7 +7,7 @@ Connects to a Victron GX gateway
 * Victron GX devices like Cerbo GX, CCGX, Venus GX
 * Victron inverter connected via VE.bus like Multis or Quattros 
 * Victron Meters
-* Connected batteries (only system registers)
+* Connected batteries
 
 ## Tested connections
 * modpoll cmd
@@ -17,6 +17,7 @@ Connects to a Victron GX gateway
 
 ## Requirements
 
+* During discovery the user must enter the UnitIDs (SlaveIDs) for VE.bus and grid connection according to his setup
 * For remote battery control the Victron inverter needs to be connected via VE.bus to the Victron GX gateway
 
 
--- victron/translations/victron-en_US.ts
+++ victron/translations/victron-en_US.ts
@@ -9,7 +9,7 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../integrationpluginvictron.cpp" line="105"/>
+        <location filename="../integrationpluginvictron.cpp" line="111"/>
         <source>The MAC address is not known. Please reconfigure the thing.</source>
         <translation></translation>
     </message>
@@ -17,7 +17,7 @@
 <context>
     <name>Victron</name>
     <message>
-        <location filename="../plugininfo.h" line="71"/>
+        <location filename="../plugininfo.h" line="68"/>
         <source>Active power</source>
         <extracomment>The name of the StateType ({0c8d0201-5036-4536-a6c0-d7c809f3afdb}) of ThingClass victronInverterTcp</extracomment>
         <translation></translation>
@@ -45,28 +45,13 @@ The name of the StateType ({e4501a2d-d148-49e0-9195-3b28ea66a53e}) of ThingClass
     </message>
     <message>
         <location filename="../plugininfo.h" line="86"/>
-        <location filename="../plugininfo.h" line="89"/>
-        <source>Battery force power timeout</source>
-        <extracomment>The name of the ParamType (ThingClass: victronBattery, ActionType: forcePowerTimeout, ID: {3017a23c-26ea-4163-9647-4f4eb2e397b6})
-----------
-The name of the StateType ({3017a23c-26ea-4163-9647-4f4eb2e397b6}) of ThingClass victronBattery</extracomment>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="92"/>
-        <source>Battery force power timeout enabled</source>
-        <extracomment>The name of the ActionType ({3017a23c-26ea-4163-9647-4f4eb2e397b6}) of ThingClass victronBattery</extracomment>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="95"/>
         <source>Battery level</source>
         <extracomment>The name of the StateType ({642b2aa3-5e97-48cb-8d93-70b1594bad43}) of ThingClass victronBattery</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="98"/>
-        <location filename="../plugininfo.h" line="101"/>
+        <location filename="../plugininfo.h" line="89"/>
+        <location filename="../plugininfo.h" line="92"/>
         <source>Battery manual mode</source>
         <extracomment>The name of the ParamType (ThingClass: victronBattery, ActionType: enableForcePower, ID: {2ae3094f-bfb7-442d-9714-474fb419414f})
 ----------
@@ -74,8 +59,8 @@ The name of the StateType ({2ae3094f-bfb7-442d-9714-474fb419414f}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="104"/>
-        <location filename="../plugininfo.h" line="107"/>
+        <location filename="../plugininfo.h" line="95"/>
+        <location filename="../plugininfo.h" line="98"/>
         <source>Battery manual mode enabled</source>
         <extracomment>The name of the ActionType ({2ae3094f-bfb7-442d-9714-474fb419414f}) of ThingClass victronBattery
 ----------
@@ -83,21 +68,21 @@ The name of the StateType ({80d5852a-c770-4803-9d1c-0013974e1246}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="110"/>
+        <location filename="../plugininfo.h" line="101"/>
         <source>Capacity</source>
         <extracomment>The name of the StateType ({e146b5af-12f8-4f14-aacc-663737d71b6e}) of ThingClass victronBattery</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="113"/>
+        <location filename="../plugininfo.h" line="104"/>
         <source>Charging state</source>
         <extracomment>The name of the StateType ({b715e1fa-f550-4d35-9858-2cf5f819286a}) of ThingClass victronBattery</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="116"/>
-        <location filename="../plugininfo.h" line="119"/>
-        <location filename="../plugininfo.h" line="122"/>
+        <location filename="../plugininfo.h" line="107"/>
+        <location filename="../plugininfo.h" line="110"/>
+        <location filename="../plugininfo.h" line="113"/>
         <source>Connected</source>
         <extracomment>The name of the StateType ({cd3c9cef-4693-48ea-aabf-2590e94207d3}) of ThingClass victronBattery
 ----------
@@ -107,32 +92,32 @@ The name of the StateType ({9d6edc96-46da-4d95-accc-fb827f9cb983}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="125"/>
+        <location filename="../plugininfo.h" line="116"/>
         <source>Current power</source>
         <extracomment>The name of the StateType ({fdf4033a-f69e-4092-a3a3-87de1d2d1428}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="128"/>
+        <location filename="../plugininfo.h" line="119"/>
         <source>Current power phase A</source>
         <extracomment>The name of the StateType ({298e7918-4320-4621-b270-ed74a5839ebe}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="131"/>
+        <location filename="../plugininfo.h" line="122"/>
         <source>Current power phase B</source>
         <extracomment>The name of the StateType ({2b51006f-7e0d-448b-b434-5932332df6a9}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="134"/>
+        <location filename="../plugininfo.h" line="125"/>
         <source>Current power phase C</source>
         <extracomment>The name of the StateType ({29479958-61f5-459e-9cf6-75542857b56c}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="137"/>
-        <location filename="../plugininfo.h" line="140"/>
+        <location filename="../plugininfo.h" line="128"/>
+        <location filename="../plugininfo.h" line="131"/>
         <source>Frequency</source>
         <extracomment>The name of the StateType ({02798ddb-4ae3-4cec-b1da-4521fae520a3}) of ThingClass victronMeter
 ----------
@@ -140,133 +125,133 @@ The name of the StateType ({7675f1ed-334c-4f1d-8983-be7e69558496}) of ThingClass
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="158"/>
-        <source>Timeout countdown</source>
-        <extracomment>The name of the StateType ({976560a2-1022-4089-9546-9bd143f5e3b2}) of ThingClass victronBattery</extracomment>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="173"/>
+        <location filename="../plugininfo.h" line="161"/>
         <source>UnitID Grid</source>
         <extracomment>The name of the ParamType (ThingClass: victronInverterTcp, Type: thing, ID: {aa633a0b-7463-404f-8e1d-f3ac86901c6a})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="176"/>
+        <location filename="../plugininfo.h" line="164"/>
         <source>UnitID System</source>
         <extracomment>The name of the ParamType (ThingClass: victronInverterTcp, Type: thing, ID: {13cdb10e-8a11-49a5-a0b3-ec48348d6150})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="179"/>
+        <location filename="../plugininfo.h" line="167"/>
         <source>UnitID VE.Bus</source>
         <extracomment>The name of the ParamType (ThingClass: victronInverterTcp, Type: thing, ID: {f67b2b37-938d-4683-a75f-db5d8396f0f3})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="182"/>
+        <location filename="../plugininfo.h" line="170"/>
         <source>VICTRON Battery</source>
         <extracomment>The name of the ThingClass ({27d2e69c-b1a2-4316-b1c7-45c117c7cd32})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="185"/>
+        <location filename="../plugininfo.h" line="173"/>
         <source>VICTRON Inverter</source>
         <extracomment>The name of the ThingClass ({96a232b3-1d24-4bc6-9be4-23996e80c8c5})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="188"/>
+        <location filename="../plugininfo.h" line="176"/>
         <source>VICTRON Meter</source>
         <extracomment>The name of the ThingClass ({9acc2570-32e2-4183-9beb-253a532f9347})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="191"/>
+        <location filename="../plugininfo.h" line="179"/>
         <source>VICTRON Solar Electric</source>
         <extracomment>The name of the vendor ({d6cc8099-a92c-4aec-ad0c-af96f874c2cf})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="194"/>
+        <location filename="../plugininfo.h" line="182"/>
         <source>Victron</source>
         <extracomment>The name of the plugin Victron ({e5e49b03-c1a9-4bf9-a6f4-733bb19d98ac})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="143"/>
+        <location filename="../plugininfo.h" line="134"/>
         <source>MAC address</source>
         <extracomment>The name of the ParamType (ThingClass: victronInverterTcp, Type: thing, ID: {4977a11c-35e7-4cb8-b993-9db4ee454ada})</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="146"/>
+        <location filename="../plugininfo.h" line="71"/>
+        <source>Battery capacity</source>
+        <extracomment>The name of the ParamType (ThingClass: victronBattery, Type: settings, ID: {36b8455c-d92b-482f-8136-45fb2f8b7bdf})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="137"/>
         <source>Phase A current</source>
         <extracomment>The name of the StateType ({8246f30f-d534-409a-abb6-110b60648ac2}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="149"/>
+        <location filename="../plugininfo.h" line="140"/>
         <source>Phase B current</source>
         <extracomment>The name of the StateType ({d996e884-70d6-41b2-8330-8495f7728851}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="152"/>
+        <location filename="../plugininfo.h" line="143"/>
         <source>Phase C current</source>
         <extracomment>The name of the StateType ({7bd46082-5137-4be1-a2a9-7b35c07ee4c3}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="155"/>
+        <location filename="../plugininfo.h" line="146"/>
         <source>Temperature</source>
         <extracomment>The name of the StateType ({0519beed-8003-4cc3-9238-33ece4e18a77}) of ThingClass victronBattery</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="161"/>
+        <location filename="../plugininfo.h" line="149"/>
         <source>Total energy produced</source>
         <extracomment>The name of the StateType ({9d127145-a1e7-42cb-819e-fb39727f1c57}) of ThingClass victronInverterTcp</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="164"/>
+        <location filename="../plugininfo.h" line="152"/>
         <source>Total imported energy</source>
         <extracomment>The name of the StateType ({788849e9-b367-4f3c-b8d6-bfdbfd560dcc}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="167"/>
+        <location filename="../plugininfo.h" line="155"/>
         <source>Total real power</source>
         <extracomment>The name of the StateType ({41dd0031-82a8-4d5b-8b68-cfe0d247eb77}) of ThingClass victronBattery</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="170"/>
+        <location filename="../plugininfo.h" line="158"/>
         <source>Total returned energy</source>
         <extracomment>The name of the StateType ({64b479ce-c70a-4cb5-8b5d-3c24d3e77bee}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="197"/>
+        <location filename="../plugininfo.h" line="185"/>
         <source>Voltage</source>
         <extracomment>The name of the StateType ({f33a8266-7b15-44fe-b2b2-20a673f35c1d}) of ThingClass victronBattery</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="200"/>
+        <location filename="../plugininfo.h" line="188"/>
         <source>Voltage phase A</source>
         <extracomment>The name of the StateType ({f01e2e3b-e58f-4c81-aa5a-14b0c46fd3aa}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="203"/>
+        <location filename="../plugininfo.h" line="191"/>
         <source>Voltage phase B</source>
         <extracomment>The name of the StateType ({6e436538-4567-4fd6-8b6f-aaf95cdb6f72}) of ThingClass victronMeter</extracomment>
         <translation></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="206"/>
+        <location filename="../plugininfo.h" line="194"/>
         <source>Voltage phase C</source>
         <extracomment>The name of the StateType ({16ca0ab7-f12d-4d44-b5a3-e65ea6164b38}) of ThingClass victronMeter</extracomment>
         <translation></translation>
