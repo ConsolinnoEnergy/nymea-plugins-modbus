a883ee0c1d8b10daa9228370f132a96525789ac3
Michael Schwarz <m.schwarz@consolinno.de>
2025-07-02 12:41:51 +0200

    Set max. value of max. charge current according to power type of wallbox after initializing.

--- solax/integrationpluginsolax.cpp
+++ solax/integrationpluginsolax.cpp
@@ -90,6 +90,29 @@ quint16 chargePhaseToPhaseCount(SolaxEvcG2ModbusTcpConnection::ChargePhase charg
     return (chargePhase == SolaxEvcG2ModbusTcpConnection::ChargePhaseThreePhase) ? 3 : 1;
 }
 
+float typePowerToMaxChargeCurrent(SolaxEvcG2ModbusTcpConnection::TypePower typePower)
+{
+    auto maxChargeCurrent = 16.f;
+    switch (typePower) {
+        case SolaxEvcG2ModbusTcpConnection::TypePower6kW:
+            maxChargeCurrent = 16.f;
+            break;
+        case SolaxEvcG2ModbusTcpConnection::TypePower7kW:
+            maxChargeCurrent = 32.f;
+            break;
+        case SolaxEvcG2ModbusTcpConnection::TypePower11kW:
+            maxChargeCurrent = 16.f;
+            break;
+        case SolaxEvcG2ModbusTcpConnection::TypePower22kW:
+            maxChargeCurrent = 32.f;
+            break;
+        case SolaxEvcG2ModbusTcpConnection::TypePower4p6kW:
+            maxChargeCurrent = 20.f;
+            break;
+        }
+    return maxChargeCurrent;
+}
+
 IntegrationPluginSolax::IntegrationPluginSolax()
 {
 
@@ -1682,6 +1705,13 @@ void IntegrationPluginSolax::setupEvcG2TcpConnection(ThingSetupInfo *info)
                     qCDebug(dcSolax()) << "Solax wallbox initialized.";
                     thing->setStateValue(solaxEvcG2FirmwareVersionStateTypeId,
                                          connection->firmwareVersion());
+                    const auto maxChargeCurrent =
+                            typePowerToMaxChargeCurrent(connection->typePower());
+                    qCDebug(dcSolax())
+                            << "Setting maximum value of max. charge current to"
+                            << maxChargeCurrent;
+                    thing->setStateMaxValue(solaxEvcG2MaxChargingCurrentStateTypeId,
+                                            maxChargeCurrent);
                     configureEvcG2(connection);
                 } else {
                     qCDebug(dcSolax()) << "Solax wallbox initialization failed.";
@@ -1693,24 +1723,7 @@ void IntegrationPluginSolax::setupEvcG2TcpConnection(ThingSetupInfo *info)
     connect(connection, &SolaxEvcG2ModbusTcpConnection::typePowerChanged, thing,
             [thing](SolaxEvcG2ModbusTcpConnection::TypePower typePower) {
                 qCDebug(dcSolax()) << "Received wallbox power type information:" << typePower;
-                auto maxChargeCurrent = 16.f;
-                switch (typePower) {
-                    case SolaxEvcG2ModbusTcpConnection::TypePower6kW:
-                        maxChargeCurrent = 16.f;
-                        break;
-                    case SolaxEvcG2ModbusTcpConnection::TypePower7kW:
-                        maxChargeCurrent = 32.f;
-                        break;
-                    case SolaxEvcG2ModbusTcpConnection::TypePower11kW:
-                        maxChargeCurrent = 16.f;
-                        break;
-                    case SolaxEvcG2ModbusTcpConnection::TypePower22kW:
-                        maxChargeCurrent = 32.f;
-                        break;
-                    case SolaxEvcG2ModbusTcpConnection::TypePower4p6kW:
-                        maxChargeCurrent = 20.f;
-                        break;
-                    }
+                const auto maxChargeCurrent = typePowerToMaxChargeCurrent(typePower);
                 qCDebug(dcSolax())
                         << "Setting maximum value of max. charge current to"
                         << maxChargeCurrent;
